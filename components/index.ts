73a44fb9ec4e27445f6e061c6d03b1a2033c8e03
Apurba Roy <apurba00237@gmail.com>
2025-04-20 20:48:33 +0530

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -3,5 +3,8 @@ import { CodeInput } from "./CodeInput";
 import { Appbar } from "./Appbar";
 import { TextInput } from "./TextInput";
 import { FileGet } from "./FileGet";
+import { Footer } from "./Footer";
+import { GetText } from "./GetText";
 
-export { FileShare, CodeInput, Appbar, TextInput , FileGet};
+
+export { FileShare, CodeInput, Appbar, TextInput, FileGet, Footer, GetText };
